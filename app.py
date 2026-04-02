10a359c4508d9f7e378e5fac278978eadfedf315
Nicolai Ommer <nicolai.ommer@qaware.de>
2026-04-02 13:30:12 +0200

    feat: Add recognizers.yaml and defaulting to local config directory

--- app.py
+++ app.py
@@ -39,9 +39,16 @@ class Server:
         self.logger.setLevel(os.environ.get("LOG_LEVEL", self.logger.level))
         self.app = Flask(__name__)
 
-        analyzer_conf_file = os.environ.get("ANALYZER_CONF_FILE")
-        nlp_engine_conf_file = os.environ.get("NLP_CONF_FILE")
-        recognizer_registry_conf_file = os.environ.get("RECOGNIZER_REGISTRY_CONF_FILE")
+        conf_dir = Path(__file__).parent / "conf"
+        analyzer_conf_file = os.environ.get(
+            "ANALYZER_CONF_FILE", str(conf_dir / "analyzer.yaml")
+        )
+        nlp_engine_conf_file = os.environ.get(
+            "NLP_CONF_FILE", str(conf_dir / "nlp.yaml")
+        )
+        recognizer_registry_conf_file = os.environ.get(
+            "RECOGNIZER_REGISTRY_CONF_FILE", str(conf_dir / "recognizers.yaml")
+        )
 
         self.logger.info("Starting analyzer engine")
         self.engine: AnalyzerEngine = AnalyzerEngineProvider(
